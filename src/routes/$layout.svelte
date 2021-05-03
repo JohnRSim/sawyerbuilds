85f5691989b43d042dd301e208b2e9c7c6496e14
John Sim <jsim@fishbowlsolutions.com>
2021-05-03 10:38:54 +0100

    test

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.css';
+</script>
+
+<style>
+</style>
+
+<main>
+	<slot />
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,27 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+	
+</script>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+</style>
+
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	Sawyer Builds Under construction.
+	
+</section>
